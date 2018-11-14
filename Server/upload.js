4def4e56761296d094f44a9c80b0a68f42fc00c8
Wonho ha <haho013039@gmail.com>
2018-11-14 16:03:32 +0900

    END

--- Server/upload.js
+++ Server/upload.js
@@ -29,8 +29,17 @@ async function list_(ctx, next) {
 router.post('/upload', async (ctx) => {
 
     const file = ctx.request.files.file;
-    console.log('uploading %s -> %s', file.name, file.path);
-    fs.rename(file.path, "uploads\\"+file.name, (err) => { })
+    const back = ctx.request.files.back;
+    const path = "C:/Bitnami/wampstack-7.1.20-1/apache2/htdocs/GSMarket/item/";
+    const req = ctx.request.body;
+    
+    fs.mkdirSync(path+req.name, { recursive: true });
+    fs.appendFileSync(path+'../item_list.txt', "/"+req.name);
+    fs.writeFileSync(path+req.name+'/developer.txt', req.dev_name, 'utf8');
+    fs.writeFileSync(path+req.name+'/interduce.txt', req.document, 'utf8');
+    fs.copyFileSync(file.path, path+req.name+'/file.zip');
+    fs.copyFileSync(back.path, path+req.name+'/back.png');
+
     await ctx.redirect('/clear');
 })
 
