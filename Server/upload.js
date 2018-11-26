510c1aeadb86ae38e4b6205f6c85641dd06afe46
Wonho ha <haho013039@gmail.com>
2018-11-26 11:14:54 +0900

    Update upload.js

--- Server/upload.js
+++ Server/upload.js
@@ -8,6 +8,17 @@ const koaBody = require('koa-body');
 const Koa = require('koa');
 const KoaStatic = require('koa-static');
 
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: '45.76.222.210',
+    user: 'root',
+    password: 'des123',
+    port: '9099',
+    database: 'gsmarket',
+    insecureAuth: true
+});
+
 const app = module.exports = new Koa();
 
 app.use(koaBody({
@@ -15,7 +26,7 @@ app.use(koaBody({
     multipart: true,
     urlencoded: true
 }));
-app.use(KoaStatic('../Client'));
+app.use(KoaStatic('../Items'));
 app.use(render);
 
 router.get('/', list);
@@ -26,21 +37,45 @@ router.get('/clear', list_);
 async function list_(ctx, next) {
     await ctx.render('clear.html');
 };
+function parseQuery(data) {
+    return [
+        [data[0], data[1], data[2], data[3], data[4]]
+    ];
+};
 router.post('/upload', async (ctx) => {
 
     const file = ctx.request.files.file;
     const back = ctx.request.files.back;
-    const path = "C:/Bitnami/wampstack-7.1.20-1/apache2/htdocs/GSMarket/item/";
+    const path = "../Items/";
     const req = ctx.request.body;
-    
-    fs.mkdirSync(path+req.name, { recursive: true });
-    fs.appendFileSync(path+'../item_list.txt', "/"+req.name);
-    fs.writeFileSync(path+req.name+'/developer.txt', req.dev_name, 'utf8');
-    fs.writeFileSync(path+req.name+'/interduce.txt', req.document, 'utf8');
-    fs.copyFileSync(file.path, path+req.name+'/file.zip');
-    fs.copyFileSync(back.path, path+req.name+'/back.png');
+    const rootURL = '45.76.222.210/'+req.name;
 
-    await ctx.redirect('/clear');
+    const SQL_ParameterValues = parseQuery([req.name, req.dev_name,req.document,rootURL+"/back.png",rootURL+'/file.zip']);
+
+    connection.query("insert into item(name, developer, interduce, image, file) values ?", [SQL_ParameterValues], async (err) => {
+        if(err)
+        {
+            console.log(err);
+            return false;
+        }
+        console.log('good');
+        fs.mkdirSync(path+req.name, { recursive: true });
+        fs.copyFileSync(file.path, path+req.name+'/file.zip');
+        fs.copyFileSync(back.path, path+req.name+'/back.png');
+        await ctx.redirect('/clear');
+    });
+    console.log('end');
+    await ctx.redirect('/error');
+})
+
+router.get('/delete',(ctx)=>{
+    const req = ctx.request.query;
+    console.log(req);
+    connection.query("delete from item where name = "+req.name,(err)=>{
+        if(err){
+            console.log(err);
+        }
+    });
 })
 
 app.use(router.routes());
