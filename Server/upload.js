d35f11862e1db310a4051a21cc93459daa546fa4
Wonho ha <haho013039@gmail.com>
2018-11-28 17:42:41 +0900

    end

--- Server/upload.js
+++ Server/upload.js
@@ -9,8 +9,8 @@ const KoaStatic = require('koa-static');
 
 var mysql = require('mysql');
 
-const upload_form = "<div class='info' style='position: absolute; top: 50%;left: 50%; transform: translateY(-50%) translateX(-50%);''><form class='was-validated' action='/upload' method='post' enctype='multipart/form-data'><div><label for='form-2-first-name'>Name</label><input type='text' name='name' class='form-control width200'></div><div class='row-md-120'><label for='form-2-first-name'>Documentation</label><textarea name='document' cols='40' rows='8' class='form-control width200'></textarea></div><div class='row-md-120'><label for='form-2-first-name'>Files</label><div class='custom-file mb-3'><input type='file' class='custom-file-input' onchange='selected(\"file_\")' name='file' multiple><label class='custom-file-label' name='file_' for='customFile'>Default custom file input</label></div></div> <div class='row-md-120'><label for='form-2-first-name'>Photo</label><div class='custom-file mb-3'><input type='file' onchange='selected(\"photo_\")' class='custom-file-input' name='back' multiple><label class='custom-file-label' name='photo_' for='customFile'>Default custom file input</label></div></div><div class='row-md-120'><input type='submit' class='btn btn-success' value='Change'></div></form></div>";
-const change_form = "<form class='was-validated' action='/change' method='post' enctype='multipart/form-data'><div class='col-md-12'><div class='row'><div class='row-md-120'><label for='form-2-first-name'>Name</label><input type='text' name='name' class='form-control width200'></div></div><div class='row'><div class='row-md-120'><label for='form-2-first-name'>Documentation</label><textarea name='document' cols='40' rows='8' class='form-control width200'>/<textarea></div></div><div class='row'><div class='row-md-120'><label for='form-2-first-name'>Files</label><div class='custom-file mb-3'><input type='file' class='custom-file-input' name='file' multiple><label class='custom-file-label' for='customFile'>Default custom file input</label></div></div></div><div class='row'><div class='row-md-120'><label for='form-2-first-name'>Photo</label><div class='custom-file mb-3'><input type='file' class='custom-file-input' name='back' multiple><label class='custom-file-label' for='customFile'>Default custom file input</label></div></div></div><div class='row'><div class='row-md-120'><input type='submit' class='btn btn-success' value='Change'></div></div></div></form>";
+const upload_form = "<form class='was-validated' action='/upload' method='post' enctype='multipart/form-data'><div><label for='form-2-first-name'>Name</label><input type='text' name='name' class='form-control width200'></div><div class='row-md-120'><label for='form-2-first-name'>Documentation</label><textarea name='document' cols='40' rows='8' class='form-control width200'></textarea></div><div class='row-md-120'><label for='form-2-first-name'>Files</label><div class='custom-file mb-3'><input type='file' class='custom-file-input' onchange='selected(\"file_\")' name='file' multiple><label class='custom-file-label' name='file_' for='customFile'>Default custom file input</label></div></div> <div class='row-md-120'><label for='form-2-first-name'>Photo</label><div class='custom-file mb-3'><input type='file' onchange='selected(\"photo_\")' class='custom-file-input' name='back' multiple><label class='custom-file-label' name='photo_' for='customFile'>Default custom file input</label></div></div><div class='row-md-120'><input type='submit' class='btn btn-success' value='Change'></div></form>";
+const change_form = "<form class='was-validated' action='/change' method='post' enctype='multipart/form-data'><div><label for='form-2-first-name'>Name</label><input type='text' name='name' class='form-control width200'></div><div class='row-md-120'><label for='form-2-first-name'>Documentation</label><textarea name='document' cols='40' rows='8' class='form-control width200'></textarea></div><div class='row-md-120'><label for='form-2-first-name'>Files</label><div class='custom-file mb-3'><input type='file' class='custom-file-input' onchange='selected(\"file_\")' name='file' multiple><label class='custom-file-label' name='file_' for='customFile'>Default custom file input</label></div></div> <div class='row-md-120'><label for='form-2-first-name'>Photo</label><div class='custom-file mb-3'><input type='file' onchange='selected(\"photo_\")' class='custom-file-input' name='back' multiple><label class='custom-file-label' name='photo_' for='customFile'>Default custom file input</label></div></div><div class='row-md-120'><input type='submit' class='btn btn-success' value='Change'></div></form>";
 
 var connection = mysql.createConnection({
     host: '45.76.222.210',
@@ -55,20 +55,21 @@ router.post('/upload', async (ctx) => {
     const rootURL = '45.76.222.210/' + req.name;
 
     const SQL_ParameterValues = parseQuery([req.name, dev_name, req.document, rootURL + "/back.png", rootURL + '/file.zip']);
-
-    connection.query("insert into item(name, developer, interduce, image, file) values ?", [SQL_ParameterValues], async (err) => {
-        if (err) {
-            console.log(err);
-            return false;
-        }
-        console.log('good');
-        fs.mkdirSync(path + req.name, { recursive: true });
-        fs.copyFileSync(file.path, path + req.name + '/file.zip');
-        fs.copyFileSync(back.path, path + req.name + '/back.png');
-        await ctx.redirect('/clear');
+    await new Promise((c, r) => {
+        connection.query("insert into item(name, developer, interduce, image, file) values ?", [SQL_ParameterValues], async (err) => {
+            if (err) {
+                console.log(err);
+                c('/error');
+            }
+            console.log('good');
+            fs.mkdirSync(path + req.name, { recursive: true });
+            fs.copyFileSync(file.path, path + req.name + '/file.zip');
+            fs.copyFileSync(back.path, path + req.name + '/back.png');
+            c('/clear');
+        });
+    }).then(url => {
+        ctx.redirect(url);
     });
-    console.log('end');
-    await ctx.redirect('/error');
 })
 
 router.post('/change', async (ctx) => {
@@ -81,21 +82,25 @@ router.post('/change', async (ctx) => {
     console.log('name : ' + dev_name);
     const rootURL = '45.76.222.210/' + req.name;
 
-    const SQL_ParameterValues = parseQuery([req.name, dev_name, req.document, rootURL + "/back.png", rootURL + '/file.zip']);
+    const sql = "update item set interduce = " + Wrapping('"', req.document) + " where name = " + Wrapping('"', req.name);
+    await new Promise((c, r) => {
+        connection.query(sql, async (err) => {
+            if (err) {
+                console.log(err);
+                c('/error');
+            }
+            if (file.size != 0) {
+                fs.copyFileSync(file.path, path + req.name + '/file.zip');
+            }
+            if (back.size != 0) {
+                fs.copyFileSync(back.path, path + req.name + '/back.png');
+            }
+            c('/clear');
+        });
+    }).then(url => {
+        ctx.redirect(url);
+    })
 
-    connection.query("insert into item(name, developer, interduce, image, file) values ?", [SQL_ParameterValues], async (err) => {
-        if (err) {
-            console.log(err);
-            return false;
-        }
-        console.log('good');
-        fs.mkdirSync(path + req.name, { recursive: true });
-        fs.copyFileSync(file.path, path + req.name + '/file.zip');
-        fs.copyFileSync(back.path, path + req.name + '/back.png');
-        await ctx.redirect('/clear');
-    });
-    console.log('end');
-    await ctx.redirect('/error');
 })
 
 function wrapping(name) {
@@ -129,13 +134,16 @@ router.get('/Login', async (ctx, next) => {
                 });
             })
                 .then((data) => {
+                    console.log("qwe");
                     console.log(data);
                     ctx.body = data;
                 })
         });
 
 })
-
+function Wrapping(sep, params) {
+    return sep + params + sep;
+}
 router.get('/delete', (ctx) => {
 
     const req = ctx.request.query;
@@ -152,14 +160,18 @@ router.get('/delete', (ctx) => {
 router.get('/ff', (ctx) => {
     ctx.body = upload_form;
 })
-router.get('/list', async(ctx) => {
-    const dev_name = "'"+ctx.cookies.get('name')+"'";
+router.get('/fff', (ctx) => {
+    ctx.body = change_form;
+})
+router.get('/list', async (ctx) => {
+    const dev_name = "'" + ctx.cookies.get('name') + "'";
     console.log(dev_name);
-    await new Promise((chain,r)=>{
-        connection.query('select * from item where developer='+dev_name,(err,row)=>{
+    await new Promise((chain, r) => {
+        connection.query('select * from item where developer=' + dev_name, (err, row) => {
+            var result = "";
             if (err) {
                 console.log(err);
-                
+
                 chain(2);
                 return;
             }
@@ -167,20 +179,24 @@ router.get('/list', async(ctx) => {
                 chain(2);
                 return;
             }
-            const html = '<div class="card" style="width: 20rem; transform:translateY(190%);">\
-            <img class="card-img-top" src="'+ row[0].image + '" alt="Card image cap">\
-            <div class="card-body">\
-              <h4 class="card-title">'+row[0].name+'</h4>\
-              <p class="card-text">'+row[0].interduce+'</p>\
-              <a href="#" class="btn btn-primary">Want Change?</a>\
-            </div>\
-          </div>';
-          chain("<div class='row'>"+html+html+html+"</div>"+"<div class='row'>");
-        })    
+            for (let index = 0; index < row.length; index++) {
+                const element = row[index];
+                const html = '<div class="card" style="width: 20rem;">\
+                <img class="card-img-top" src="http://'+ element.image + '" alt="Card image cap">\
+                <div class="card-body">\
+                  <h4 class="card-title">'+ element.name + '</h4>\
+                  <p class="card-text">'+ element.interduce + '</p>\
+                  <div onclick="OpenForm_change('+ Wrapping('\'', element.name) + "," + Wrapping('\'', element.interduce) + ')" class="btn btn-primary">Want Change?</div>\
+                </div>\
+              </div>';
+                result += html;
+            }
+            chain("<div class='row'>" + result + "</div>");
+        })
     }).
-    then(data=>{
-        ctx.body = data;
-    })
+        then(data => {
+            ctx.body = data;
+        })
 })
 app.use(router.routes());
 
